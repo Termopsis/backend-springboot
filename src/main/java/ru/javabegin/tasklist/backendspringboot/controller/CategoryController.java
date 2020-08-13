1598ed11f0b569b970596acb563becb99e16ab7a
Termopsis <leo_dialog@mail.ru>
2020-08-13 22:03:19 +0700

    added find by id added delete by id

--- src/main/java/ru/javabegin/tasklist/backendspringboot/controller/CategoryController.java
+++ src/main/java/ru/javabegin/tasklist/backendspringboot/controller/CategoryController.java
@@ -1,12 +1,16 @@
 package ru.javabegin.tasklist.backendspringboot.controller;
 
+import com.sun.org.apache.regexp.internal.RE;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.javabegin.tasklist.backendspringboot.entity.Category;
+import ru.javabegin.tasklist.backendspringboot.entity.Priority;
 import ru.javabegin.tasklist.backendspringboot.repo.CategoryRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Termopsis on 11.08.2020.
@@ -55,6 +59,37 @@ public class CategoryController {
         }
 
         return ResponseEntity.ok(categoryRepository.save(category));
+    }
+
+    @GetMapping("find/id/{id}")
+    public ResponseEntity<Category> findById(@PathVariable Long id){
+
+        Category category = null;
+
+        try {
+            category = categoryRepository.findById(id).get();
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+            return new ResponseEntity("element with id = "+id+" not found",HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(category);
+
+    }
+
+    @DeleteMapping("/delete/id/{id}")
+    public ResponseEntity<Category> deleteById(@PathVariable Long id){
+
+        Category category = null;
+
+        try {
+            categoryRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity("category with id = "+id+" not found",HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return new ResponseEntity(HttpStatus.OK);
+
     }
 
 }
--- src/main/java/ru/javabegin/tasklist/backendspringboot/controller/PriorityController.java
+++ src/main/java/ru/javabegin/tasklist/backendspringboot/controller/PriorityController.java
@@ -2,14 +2,16 @@ package ru.javabegin.tasklist.backendspringboot.controller;
 
 import com.sun.org.apache.regexp.internal.RE;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import ru.javabegin.tasklist.backendspringboot.entity.Category;
+import ru.javabegin.tasklist.backendspringboot.entity.Priority;
 import ru.javabegin.tasklist.backendspringboot.entity.Priority;
 import ru.javabegin.tasklist.backendspringboot.repo.PriorityRepository;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 /**
  * Created by Termopsis on 10.08.2020.
@@ -67,5 +69,34 @@ public class PriorityController {
         return ResponseEntity.ok(priorityRepository.save(priority));
     }
 
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Priority> findById(@PathVariable Long id){
 
+        Priority priority = null;
+
+        try {
+            priority = priorityRepository.findById(id).get();
+        }catch (NoSuchElementException e){
+            e.printStackTrace();
+            return new ResponseEntity("id = "+id+" not found", HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return ResponseEntity.ok(priority);
+    }
+    
+    @DeleteMapping("/delete/id/{id}")
+    public ResponseEntity<Priority> deleteById(@PathVariable Long id){
+
+        Priority priority = null;
+
+        try {
+            priorityRepository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            return new ResponseEntity("priority with id = "+id+" not found",HttpStatus.NOT_ACCEPTABLE);
+        }
+
+        return new ResponseEntity("delete is successful",HttpStatus.OK);
+
+    }
+    
 }
