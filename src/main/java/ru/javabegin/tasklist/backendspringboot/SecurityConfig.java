32dfa67950a92fa57f771f00fc6f06c0d4200648
Termopsis <leo_dialog@mail.ru>
2020-08-29 18:02:52 +0300

    spring security. added access port 4200

---
+++ src/main/java/ru/javabegin/tasklist/backendspringboot/SecurityConfig.java
@@ -0,0 +1,26 @@
+package ru.javabegin.tasklist.backendspringboot;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by Termopsis on 29.08.2020.
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/**").permitAll();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("*");
+    }
+
+}
