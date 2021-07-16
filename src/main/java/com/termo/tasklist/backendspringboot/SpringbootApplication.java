68a77e2c3ee78924a3315d0194d3d31d3baed975
MyTestGitRoman <romanfobosmir2@gmail.com>
2021-07-16 17:03:06 +0300

    Rename main/test classes

--- src/main/java/com/termo/tasklist/backendspringboot/SpringbootApplication.java
+++ src/main/java/com/termo/tasklist/backendspringboot/SpringbootApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendSpringbootApplication {
+public class SpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendSpringbootApplication.class, args);
+        SpringApplication.run(SpringbootApplication.class, args);
     }
 
 }
